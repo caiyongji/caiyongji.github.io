9ec7da3cb03027f540a14b5257ec9963b6132d08
caiyongji <i@caiyongji.com>
2024-10-09 15:12:52 +0800

    use relative image in metadata

--- caiyongji-com/app/metadata.ts
+++ caiyongji-com/app/metadata.ts
@@ -1,4 +1,3 @@
-import type { Metadata } from 'next'
 
 export const metadata = {
     title: "Cai Yongji - All about AI, Self Improvement, Startup, and Personal Growth.",
@@ -10,7 +9,7 @@ export const metadata = {
       siteName: 'Cai Yongji',
       images: [
         {
-          url: 'https://caiyongji-github-io.vercel.app/images/og-image-ai.png',
+          url: '/images/og-image-ai.png',
           width: 1298,
           height: 845,
           alt: 'Og Image Alt',
@@ -23,6 +22,6 @@ export const metadata = {
       card: 'Cai Yongji',
       title: "Cai Yongji - All about AI, Self Improvement, Startup, and Personal Growth.",
       description: "Hi, I'm Cai, an indie developer exploring AI-powered entrepreneurship. I'm taking a Build in Public approach, focusing on AI, Self Improvement, Startup, and Personal Growth.",    
-      images: ['https://caiyongji-github-io.vercel.app/images/og-image-ai.png'],
+      images: ['/images/og-image-ai.png'],
     },
   };
