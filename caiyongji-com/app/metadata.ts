d0e2386735e50324557bfe745b821e3d6628794b
caiyongji <i@caiyongji.com>
2024-10-09 15:15:54 +0800

    fix twitter card

--- caiyongji-com/app/metadata.ts
+++ caiyongji-com/app/metadata.ts
@@ -19,7 +19,7 @@ export const metadata = {
       type: 'website',
     },
     twitter: {
-      card: 'Cai Yongji',
+      card: 'summary_large_image',
       title: "Cai Yongji - All about AI, Self Improvement, Startup, and Personal Growth.",
       description: "Hi, I'm Cai, an indie developer exploring AI-powered entrepreneurship. I'm taking a Build in Public approach, focusing on AI, Self Improvement, Startup, and Personal Growth.",    
       images: ['/images/og-image-ai.png'],
